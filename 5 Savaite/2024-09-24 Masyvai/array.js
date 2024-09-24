0d42ecd1c220771d3b0d55fd41764f21267ee0dc
ElanasB <elanasbar@gmail.com>
2024-09-24 11:44:44 +0300

    array

---
+++ 5 Savaite/2024-09-24 Masyvai/array.js
@@ -0,0 +1,6 @@
+// Masyvo tikslas : talpinti tokius pacius elementus viename kintamajame.
+const array1 = [];
+const array2 = ['Tekstas1', 'Tekstas2'];
+const array3 = new Array(4); // sukuria 4 tuscius masyvus
+const array4 = new Array(10).fill(0); // [0,0,0,0,0,0,0,0,0,0]
+const myPokemon = ['asa', 'fdffd', 'hgrv', true, 8, [1, 2, 3]]; // gali laikyti skirtingus tipus.
