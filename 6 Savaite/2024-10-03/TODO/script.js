6872830add8d6b2592219dd7a8b0df8da567b855
ElanasB <elanasbar@gmail.com>
2024-10-04 13:40:46 +0300

    Team-Work-Day2

--- 6 Savaite/2024-10-03/TODO/script.js
+++ 6 Savaite/2024-10-03/TODO/script.js
@@ -4,18 +4,12 @@ let editingTaskIndex = 0;
 
 // LocalStorage gavimas
 function getTodoTasks() {
-  // Čia tūrėtumėte padaryti:
-  // 1. Gauti sąrašą iš localStorage ir jį gražinti
   const input = localStorage.getItem('taskList');
-  console.log(input);
   return input ? JSON.parse(input) : [] ;
 }
 
 // LocalStorage išsaugojimas
-// Išsaugoti perduotą data (pavyzdžiui, į LocalStorage ar kitaip)localStorage.setItem('todoTasks', JSON.stringify(data));
 function saveTodoTasks(data) {
-  // Čia tūrėtumėte padaryti:
-  // 1. Pridėti data į localStorage.
 localStorage.setItem('taskList', JSON.stringify(data));
 }
 
@@ -34,16 +28,15 @@ function addNewTodoTask() {
 
   const taskList = getTodoTasks();
   taskList.push(data);
-  console.log(taskList);
   saveTodoTasks(taskList);
-
+  updateTodoList();
 }
   // Čia tūrėtumėte padaryti:
   // 2. Patikrinti, kad inputas nebūtų tuščias. DONE
-  // 3. Gauti value iš select pasirinkto varianto - (#assingListOption - select elementas). 
-  // 4. Patikrinti, kad select nebūtų tuščias. 
-  // 5. Gauti iš funkcijos getTodoTasks sąrašą iš localStorage 
-  // 6. Pridėti naują užduotį į localStorage naudojant funkcija saveTodoTasks(data) -- data siunčiame užduoties masyva
+  // 3. Gauti value iš select pasirinkto varianto - (#assingListOption - select elementas). DONE
+  // 4. Patikrinti, kad select nebūtų tuščias. DONE
+  // 5. Gauti iš funkcijos getTodoTasks sąrašą iš localStorage DONE
+  // 6. Pridėti naują užduotį į localStorage naudojant funkcija saveTodoTasks(data) -- data siunčiame užduoties masyva DONE
   // 7. Atnaujiname sąraša
 
 
@@ -54,10 +47,65 @@ function selectDay(day, dayText, element) {
   // 2. Uždėti tekstą pagal parametra dayText į elementą (.selectedDayText)
   // 3. Atnaujinti selectedDay kintamajį į pasirinktą dieną
   // 4. Atnaujinti sąrašą
+  const weekListElement = document.querySelector('.weekListElement');
+  
 }
+selectDay('monday', 'Pirmadienio', this);
 
 // Atnaujinti sąraša į HTML
 function updateTodoList() {
+  let getTodo = getTodoTasks();
+  const listContainerElement = document.querySelector('.listContainer');
+  const values = document.querySelector('#assingListOption');
+  const selectDayText = document.querySelector('.selectedDayText');
+  let htmlText = '';
+  let divBlock;
+  
+  for(let i in getTodo) {
+    if(getTodo === undefined) {
+      return;
+    }
+    
+    let todoInput = getTodo[i].input;
+
+    divBlock = listContainerElement.innerHTML = htmlText += `  
+    <div class="todo">
+    <article>
+        <input type="checkbox" onclick="markAsDoneTask(${getTodo[i].id})"/>
+        <span>${todoInput}</span>
+    </article>
+    <div class="btnContainer">
+        <button onclick="editTodoTask(${getTodo[i].id})">
+        <i class="fa-solid fa-pen-to-square blue"></i>
+        </button>
+        <button onclick="deleteTodoTask(${getTodo[i].id})"><i class="fa-solid fa-trash red"></i></button>
+    </div>
+  </div>`
+
+    if(values.value === 'monday') {
+      selectDayText.innerText = 'Pirmadienis';
+      divBlock;
+    }
+    if(values.value === 'tuesday') {
+      selectDayText.innerText = 'Antradienis';
+      divBlock;
+    }
+    if(values.value === 'wednesday') {
+      selectDayText.innerText = 'Treciadienis';
+      divBlock;
+    }
+    if(values.value === 'thursday') {
+      selectDayText.innerText = 'Ketvirtadienis';
+      divBlock;
+    }
+    if(values.value === 'friday') {
+      selectDayText.innerText = 'Penktadienis';
+      divBlock;
+    }
+
+}
+
+
   // Čia tūrėtumėte padaryti:
   // 1. Išskaidyti localStorage masyva bei patikrinti kad jis nebūtų null.
   // 2. Patikrinti kokia diena yra pasirinkta ir pagal ją išdėsti sąraša.
