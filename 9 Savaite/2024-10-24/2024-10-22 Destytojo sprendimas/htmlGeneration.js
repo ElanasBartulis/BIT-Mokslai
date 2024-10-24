abb1c78f737fc0d0fa2e7b30f80000a65ffc1cb2
ElanasB <elanasbar@gmail.com>
2024-10-24 12:28:32 +0300

    Destytojo sprendimas

---
+++ 9 Savaite/2024-10-24/2024-10-22 Destytojo sprendimas/htmlGeneration.js
@@ -0,0 +1,36 @@
+// {
+// 	author: "Benas Lyris";
+// 	id: 6;
+// 	img: "https://in3.dev/knygos/imgs/1600844302_Kiekvienos_dienos_terapija.jpg";
+// 	price: 5.22;
+// 	time: 1729670025;
+// 	title: "Kiekvienos dienos terapija";
+// 	type: {
+//      id: 1
+//      title: "seimai ir vaikams"
+//  };
+// }
+function generateAllBooks(books) {
+  let html = "";
+  for (const book of books) {
+    html += generateBook(book);
+  }
+  return html;
+}
+function generateBook(book) {
+  return `<div class="book">
+					<img
+						src="${book.img}"
+					/>
+					<div class="details">
+						<h3>${book.title}</h3>
+						<p><b>Kategorija:</b><span>${book.type.title}</span></p>
+						<p><b>Data:</b><span>${generateDate(book.time)}</span></p>
+						<p><b>Autorius:</b><span>${book.author}</span></p>
+					</div>
+					<div class="price">
+						<h4>${book.price.toFixed(2)}€</h4>
+						<button>Prideti i krepseli</button>
+					</div>
+				</div>`;
+}
---
+++ 9 Savaite/2024-10-24/2024-10-22 Destytojo sprendimas/index.js
@@ -0,0 +1,33 @@
+async function main() {
+  const booksAndTypes = await getBooksAndBookTypesFromApi();
+  // const booksWithoutTypes = booksAndTypes.books;
+  // const types = booksAndTypes.types;
+  // Alternatyva:
+  const { books: booksWithoutTypes, types } = booksAndTypes;
+  // Knygų su tipais atradimas
+  console.log(booksWithoutTypes);
+  console.log(types);
+  // const programosNusprestasPavadinimas = "asdfghjkl asd asd:@#:$@#:$@#%^^";
+  const booksWithTypes = booksWithoutTypes.map((knyga) => ({
+    ...knyga,
+    type: types.find((type) => type.id === knyga.type),
+  }));
+  // 'Lorem ipsum'
+  const booksElement = document.querySelector(".books");
+  booksElement.innerHTML = generateAllBooks(booksWithTypes);
+
+  // Gaunamo obj. pavyzdys:
+  // {
+  // 	author: "Benas Lyris";
+  // 	id: 6;
+  // 	img: "https://in3.dev/knygos/imgs/1600844302_Kiekvienos_dienos_terapija.jpg";
+  // 	price: 5.22;
+  // 	time: 1729670025;
+  // 	title: "Kiekvienos dienos terapija";
+  // 	type: {
+  //      id: 1
+  //      title: "seimai ir vaikams"
+  //  };
+  // }
+}
+main(); //pradzios taskas
