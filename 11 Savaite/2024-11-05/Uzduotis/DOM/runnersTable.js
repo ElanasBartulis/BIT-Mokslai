74dc6a2fb2ee0f57928a5333d8a5c3e3fb013519
ElanasB <elanasbar@gmail.com>
2024-11-05 18:13:12 +0200

    Done'ish

---
+++ 11 Savaite/2024-11-05/Uzduotis/DOM/runnersTable.js
@@ -0,0 +1,24 @@
+{
+  /* <tr>
+					<td>1.</td>
+					<td>Tomas</td>
+					<td>Janušauskas</td>
+					<td>36</td>
+					<td>82kg</td>
+					<td>III</td>
+				</tr> */
+}
+
+const tableBody = document.querySelector("#table-body");
+export function addRunnerToTable(runner) {
+  // reikia ID, svorioKategorija
+  tableBody.innerHTML += `<tr>
+					<td>${runner.id}.</td>
+					<td>${runner.name}</td>
+					<td>${runner.secondName}</td>
+					<td>${runner.age}</td>
+					<td>${runner.weight}kg</td>
+					<td>${runner.category}</td>
+					<td><i data-id="${runner.id}" class="bi bi-trash delete"></i></td>
+				</tr>`;
+}
