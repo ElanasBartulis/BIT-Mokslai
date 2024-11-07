9d8be8341c01f78b3ea9d0d33b7f8af51ef97e63
ElanasB <elanasbar@gmail.com>
2024-11-07 09:58:20 +0200

    2024-11-06

---
+++ 11 Savaite/2024-11-06/index.js
@@ -0,0 +1,9 @@
+const elanas = {
+  code: [JavaScript, HTML, CSS],
+  askMeAbout: ["web dev", "tech", "music", "matcha tea"],
+  technologies: {
+    frontEnd: ["React"],
+    backEnd: ["Node.js"],
+    funFact: "One man’s crappy software is another man’s full-time job.",
+  },
+};
