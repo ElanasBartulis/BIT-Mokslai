3071df674fdc6d1ea7c059a0b466cef5861828e5
ElanasB <elanasbar@gmail.com>
2024-11-20 11:36:51 +0200

    2024-11-20

--- 13 Savaite/2024-11-19/index.js
+++ 13 Savaite/2024-11-19/index.js
@@ -1,37 +1,37 @@
 import connection from "./mysql-connect.js";
 
 async function getAllUsers() {
-	const sql = "SELECT * FROM `users`";
-	const [data] = await connection.query(sql);
-	return data;
+  const sql = "SELECT * FROM `users`";
+  const [data] = await connection.query(sql);
+  return data;
 }
 async function getOneUser(id) {
-	const sql = `SELECT * FROM users WHERE id = ${id}`;
-	const [data] = await connection.query(sql);
-	return data[0];
+  const sql = `SELECT * FROM users WHERE id = ${id}`;
+  const [data] = await connection.query(sql);
+  return data[0];
 }
 async function insertIntoUsers(user) {
-	const sql = `INSERT INTO users (username, password) VALUES('${user.username}', '${user.password}')`;
-	const response = await connection.query(sql);
-	console.log(response);
-	// INSERT INTO table_name (column1, column2, column3, ...)
-	// VALUES (value1, value2, value3, ...);
+  const sql = `INSERT INTO users (username, password) VALUES('${user.username}', '${user.password}')`;
+  const response = await connection.query(sql);
+  console.log(response);
+  // INSERT INTO table_name (column1, column2, column3, ...)
+  // VALUES (value1, value2, value3, ...);
 }
 async function updateUser(id, updatedFields) {
-	const fieldsToUpdate = Object.keys(updatedFields)
-		.map((key) => `${key} = '${updatedFields[key]}'`)
-		.join(", ");
+  const fieldsToUpdate = Object.keys(updatedFields)
+    .map((key) => `${key} = '${updatedFields[key]}'`)
+    .join(", ");
 
-	const sql = `UPDATE users SET ${fieldsToUpdate} WHERE id = ${id}`;
-	const data = await connection.query(sql);
-	console.log(data);
-	return data;
+  const sql = `UPDATE users SET ${fieldsToUpdate} WHERE id = ${id}`;
+  const data = await connection.query(sql);
+  console.log(data);
+  return data;
 }
 async function deleteUser(id) {
-	const sql = `DELETE FROM users WHERE id = ${id}`;
-	const data = await connection.query(sql);
-	console.log(data);
-	return data;
+  const sql = `DELETE FROM users WHERE id = ${id}`;
+  const data = await connection.query(sql);
+  console.log(data);
+  return data;
 }
 // {
 // username: "",
@@ -54,5 +54,8 @@ async function deleteUser(id) {
 // console.log(user);
 
 // Naudotojo atnaujinimas
-// await updateUser(7, { username: "Julius", password: "naujasJuliausSlaptazodis" });
+// await updateUser(7, {
+//   username: "Julius",
+//   password: "naujasJuliausSlaptazodis",
+// });
 // await deleteUser(8);
