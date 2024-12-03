27e350facc3a586cfacc645a1fcc73728faf8875
ElanasB <elanasbar@gmail.com>
2024-12-03 16:18:53 +0200

    2024-12-03

---
+++ 15 Savaite/2024-12-03/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Login</Button>
+      <Button>Register</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ 15 Savaite/2024-12-03/vite-project/src/components/ui/button.jsx
@@ -0,0 +1,3 @@
+export default function Button(props) {
+  return <button className="bg-green-500">{props.children}</button>;
+}
